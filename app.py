a528c0510b118c5d895dfabeb565a961525ff280
Adam Muhammed <deensname@gmail.com>
2023-07-10 10:51:26 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
+
+# Define a form for user authentication
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log in')
+
+# Define a form for TTS inference
+class InferenceForm(FlaskForm):
+    text_input = StringField('Text Input', validators=[DataRequired()])
+    submit = SubmitField('Generate Speech')
+
+# Define your TTS model inference function
+def generate_speech(text_input):
+    # Load your TTS model
+    # Run inference with the provided text_input
+    # Return the generated speech
+
+# Define your routes and views
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Perform authentication checks here (e.g., username and password validation)
+        return redirect(url_for('inference'))
+    return render_template('login.html', form=form)
+
+@app.route('/inference', methods=['GET', 'POST'])
+def inference():
+    form = InferenceForm()
+    if form.validate_on_submit():
+        text_input = form.text_input.data
+        speech_output = generate_speech(text_input)
+        # Play or save the generated speech output
+
+    return render_template('inference.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
